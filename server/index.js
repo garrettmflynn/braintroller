2e0daee2f6f653dfa30096bf4bb9dccb8280b5b3
Garrett Michael Flynn <46533749+GarrettMFlynn@users.noreply.github.com>
2023-02-04 15:00:47 -0800

    Network check enabled

--- server/index.js
+++ server/index.js
@@ -1,16 +1,12 @@
 import robot from 'robotjs'
+import './network.js' // Print the network you're on
 
 import { WebSocketServer } from 'ws';
+
 
 const wss = new WebSocketServer({ port: 8765 });
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
-
-  ws.on('message',  (data) => {
-    console.log('received: %s', data);
-    robot.keyTap(data)
-  });
-
-//   ws.send('something');
+  ws.on('message', robot.keyTap);
 });
