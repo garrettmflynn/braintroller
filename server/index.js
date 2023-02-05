6d2040039136b1cc02514b0220ec2eede06c8b93
Garrett Michael Flynn <46533749+GarrettMFlynn@users.noreply.github.com>
2023-02-05 09:05:00 -0800

    Include connection message on server

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: 8765 });
 
 wss.on('connection', function connection(ws) {
+
+  console.log('Client connected!\n')
+
   ws.on('error', console.error);
   ws.on('message', (message) => {
     const { id, command, payload } = JSON.parse(message)
